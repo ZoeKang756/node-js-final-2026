4802481b388270bcb9f3fe020085a00cd18061b8
zoe.kang <jujuhwa@gmail.com>
2026-09-15 21:23:44 +0800

    pino-pretty 載入時機調整

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,11 +1,19 @@
 const pino = require('pino')
-const pretty = require('pino-pretty')
 
-module.exports = function getLogger (prefix, logLevel = 'debug') {
-  return pino(pretty({
+module.exports = function getLogger(prefix, logLevel = 'debug') {
+  const isProd = process.env.NODE_ENV === 'production'
+
+  return pino({
     level: logLevel,
     messageFormat: `[${prefix}]: {msg}`,
-    colorize: true,
-    sync: true
-  }))
+    transport: isProd
+      ? undefined
+      : {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            sync: true,
+          },
+        },
+  })
 }
